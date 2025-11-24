9b5497f0c5caba0a3b25c1cd1cc48934eb51df67
CoderXYZ14 <s1404islam@gmail.com>
2025-11-25 04:24:30 +0530

    Auth check added

--- src/routes/approve.routes.ts
+++ src/routes/approve.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { approveLogin } from "../controllers/approve.controller";
+import { verifyJWT } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.post("/", asyncHandler(approveLogin));
+router.post("/", verifyJWT, asyncHandler(approveLogin));
 
 export default router;
